1347b85e4df07a8a25276ea03f5af0b871a390ed
Leandro Ferreira <leandro.daf4@gmail.com>
2021-02-14 11:16:53 -0300

    fix mercado pago tested

--- tests/Unit/Services/Payments/GatewayMethodTest.php
+++ tests/Unit/Services/Payments/GatewayMethodTest.php
@@ -4,6 +4,7 @@ use App\Exceptions\ValidatorException;
 use App\Models\Item;
 use App\Services\Payments\GatewayMethod;
 use App\Services\Payments\MercadoPagoGateway;
+use Illuminate\Support\Facades\Config;
 
 class GatewayMethodTest extends TestCase
 {
@@ -12,6 +13,8 @@ class GatewayMethodTest extends TestCase
         $gateway = 'mercado-pago';
         $item = Item::factory()->make();
         $data = [];
+
+        Config::set('payment.mercadopago.secret', 'TEST-3333644868245781-020502-2cf74afc663e26a3bd176206c0b749ed-205648376');
 
         $result = $this->app->make(GatewayMethod::class)->get($gateway, $item, $data);
 
